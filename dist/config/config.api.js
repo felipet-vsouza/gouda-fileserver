6ac5cbbf6d527cd968a3af90be587798f1830fc7
Felipe Souza <felipe.souza@cwi.com.br>
2017-02-12 22:39:44 -0200

    Fix: remove dist/ from repo

--- dist/config/config.api.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Configuration;
-(function (Configuration) {
-    function loadConfiguration() {
-        var config = require('./config.json');
-        return config;
-    }
-    Configuration.loadConfiguration = loadConfiguration;
-})(Configuration = exports.Configuration || (exports.Configuration = {}));
--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var server = require("./server.core");
-var configAPI = require("./config/config.api");
-(function startServer() {
-    var serverInstance = server.ServerCore.createServer();
-    var serverConfig = configAPI.Configuration.loadConfiguration();
-    serverInstance.listen(serverConfig.server.ports.fileserver);
-})();
