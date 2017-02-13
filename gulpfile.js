92c7c950ae4dcc3a5dd5009eab98c6834812798c
Felipe Souza <felipe.souza@cwi.com.br>
2017-02-12 22:36:00 -0200

    Feat: create Gulp tasks to copy assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var configJSON = "src/core/config/config.json";
 
 gulp.task("default", function () {
+    gulp.start("build", "copyAssets");
+});
+
+gulp.task("build", function () {
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+        .js.pipe(gulp.dest("dist/"));
+});
+
+gulp.task("copyAssets", function () {
+    gulp.start('copyConfigJSON');
+});
+
+gulp.task("copyConfigJSON", () => {
+    gulp.src(configJSON)
+        .pipe(gulp.dest("dist/config"));
 });
